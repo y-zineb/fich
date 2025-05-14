b3fee3cf8ba96667237113b61d3c8e7db80a2cd5
y-zineb <zineb21511@gmail.com>
2025-05-14 21:18:17 +0100

    dist

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../App.css";
 import "../styles/Accueil.css";
 import BannerImage from "../assets/home.png";
 
--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
-import { Activitygrid } from '../helpers/Activitygrid';
-import Activitycard from '../components/Activitycard';
-import { Projectgrid } from '../helpers/Projectgrid';
-import Projectcard from '../components/Projectcard';
-import '../styles/Activities.css';
+import React from "react";
+import { Activitygrid } from "../helpers/Activitygrid";
+import Activitycard from "../components/Activitycard";
+import { Projectgrid } from "../helpers/Projectgrid";
+import Projectcard from "../components/Projectcard";
+import "../styles/Activities.css";
 
 function Activities() {
   return (
     <div className="Mission">
-        <div className="Activities">
-            <h2 className="Title">Nos Activités</h2>
-            <div className="Activitygrid">
-                {Activitygrid.map((a, index) => {
-                    return <Activitycard image={a.image} name={a.name} description={a.description} />
-                    })}
-            </div>
+      <div className="Activities">
+        <h2 className="Title">Nos Activités</h2>
+        <div className="Activitygrid">
+          {Activitygrid.map((a, index) => {
+            return (
+              <Activitycard
+                image={a.image}
+                name={a.name}
+                description={a.description}
+              />
+            );
+          })}
         </div>
-        <div className="Projects">
-            <h2 className="Title">Nos Projets</h2>
-            <div className="Projectgrid">
-                {Projectgrid.map((p, index) => {
-                    return <Projectcard image={p.image} name={p.name} description={p.description} />
-                    })}
-            </div>
+      </div>
+      <div className="Projects">
+        <h2 className="Title">Nos Projets</h2>
+        <div className="Projectgrid">
+          {Projectgrid.map((p, index) => {
+            return (
+              <Projectcard
+                image={p.image}
+                name={p.name}
+                description={p.description}
+              />
+            );
+          })}
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Activities
+export default Activities;
